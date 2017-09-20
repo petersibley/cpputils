763409575410eadb41e3c332af646adbd8154215
Peter Sibley <peter@fiftythree.com>
2017-09-20 09:56:47 -0400

    ws

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
+#include "stringutils.h"
 #include <iostream>
 #include <string>
-#include "stringutils.h"
 
 using std::string;
 using namespace cpputils;
 
-int main() 
+int main()
 {
-    auto s = string{"  test"};
-    ltrim(s); 
-    
-    auto t = string{"test  "};
-    rtrim(t); 
+    auto s = string{ "  test" };
+    ltrim(s);
 
-    std::cout << s << "\n" << t << "\n";
+    auto t = string{ "test  " };
+    rtrim(t);
+
+    std::cout << s << "\n"
+              << t << "\n";
     return 0;
 }
--- src/stringutils.h
+++ src/stringutils.h
@@ -1,19 +1,21 @@
 #pragma once
-#include <string>
 #include <algorithm>
+#include <string>
 
-namespace cpputils
+namespace cpputils {
+inline void ltrim(std::string& s)
 {
-    inline void ltrim(std::string &s) {
-        s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int c) {
-                    return !std::isspace(c);
-        }));
-    }
-
-    inline void rtrim(std::string &s) {
-        s.erase(std::find_if(s.rbegin(), s.rend(), [](int c) {
-                    return !std::isspace(c);
-        }).base(), s.end());
-    }
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int c) {
+        return !std::isspace(c);
+    }));
 }
 
+inline void rtrim(std::string& s)
+{
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](int c) {
+        return !std::isspace(c);
+    })
+                .base(),
+        s.end());
+}
+}
